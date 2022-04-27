030b5d424da289c96931fb8db23e3b3d070f7ea3
Ayyappa Swamy Angadi <ayyappaswamy50@gmail.com>
2022-04-27 14:03:47 +0530

    done
    done

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,7 +4,7 @@ import React from 'react'
 
 const Welcome = (props) => {
   return (
-    <div>Hey ! <h1>{props.name}</h1>
+    <div>Hey !<h1>{props.name}</h1>
         <h2>Welcome to Newton School.</h2>
     </div>
   )
